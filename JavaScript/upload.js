8b55bae56510fbe96b60ae6aeacf573c8c688fda
Yarred097 <yaderisaac07@gmail.com>
2021-06-08 22:14:48 -0600

    Agregar Calendario Completado

---
+++ JavaScript/upload.js
@@ -0,0 +1,10 @@
+
+
+
+var modal = document.getElementById('upload');
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
--- main/precarga.js
+++
@@ -1,4 +0,0 @@
-window.onload = function(){
-    $('#precargado').fadeOut();
-    $('body').removeClass('cuerpo');
-}
